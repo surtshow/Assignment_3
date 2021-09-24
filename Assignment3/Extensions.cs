e0f2f75252d8641563e03211732f601c89532edb
Daniel <dlli@itu.dk>
2021-09-24 13:04:32 +0200

    added flatten method

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BDSA2021.Assignment3
 {
     public static class Extensions
     {
+        public static IEnumerable<T> Flatten<T> (this IEnumerable<IEnumerable<T>> items)
+        => throw new NotImplementedException();
+
     }
 }
