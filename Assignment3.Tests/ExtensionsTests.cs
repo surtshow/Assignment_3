2ff80b87ded31325314e4c581c1137009987cd28
Jonas Lindvig <jonli@itu.dk>
2021-09-24 13:07:47 +0200

    Add flatten unit test

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace BDSA2021.Assignment3.Tests
 {
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
+        public void Flatten_given_1_2_and_3_4_return_1_2_3_4()
         {
+            IEnumerable<int> innerlist1 = new List<int>{1,2};
+            IEnumerable<int> innerlist2 = new List<int>{3,4};
+            IEnumerable<int>[] outerlist = {innerlist1, innerlist2};
 
+            IEnumerable<int> actuel = outerlist.Flatten();
+
+            IEnumerable<int> expected = new List<int>{1,2,3,4};
+            Assert.Equal(expected, actuel);
         }
     }
 }
