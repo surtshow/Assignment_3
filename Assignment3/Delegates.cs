333a6d8157cd3165416f50afb4c1ebef321bcd61
Daniel <dlli@itu.dk>
2021-09-27 16:29:06 +0200

    Implemented delegates

--- Assignment3/Delegates.cs
+++ Assignment3/Delegates.cs
@@ -1,8 +1,15 @@
 using System;
+using System.Linq;
 
 namespace BDSA2021.Assignment3
 {
     public class Delegates
     {
+        Action<string> ReverseString = s => Console.WriteLine(new string(s.Reverse().ToArray()));
+        
+        Converter<(double,double),double> Product = c => c.Item1 * c.Item2;
+
+        Predicate<(string,int)> NumEqual = num => int.Parse(num.Item1) == num.Item2; 
+
     }
 }
