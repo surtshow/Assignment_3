c5bf2c09220a4a99f8c81513ffc31f3ead31d130
Daniel <dlli@itu.dk>
2021-09-27 16:34:19 +0200

    made methods public

--- Assignment3/Delegates.cs
+++ Assignment3/Delegates.cs
@@ -5,11 +5,11 @@ namespace BDSA2021.Assignment3
 {
     public class Delegates
     {
-        Action<string> ReverseString = s => Console.WriteLine(new string(s.Reverse().ToArray()));
+        public Action<string> ReverseString = s => Console.WriteLine(new string(s.Reverse().ToArray()));
         
-        Converter<(double,double),double> Product = c => c.Item1 * c.Item2;
+        public Converter<(double,double),double> Product = c => c.Item1 * c.Item2;
 
-        Predicate<(string,int)> NumEqual = num => int.Parse(num.Item1) == num.Item2; 
+        public Predicate<(string,int)> NumEqual = num => int.Parse(num.Item1) == num.Item2; 
 
     }
 }
