edbbc8851f71246a26ebf765919df4b66522caa8
Jonas Lindvig <48011853+surtshow@users.noreply.github.com>
2021-09-29 13:23:46 +0200

    Linq (#6)
    * Extensions implementation (#3)
    
    * Fixed namespace
    
    * created flatten method
    
    Co-authored-by: Daniel <dlli@itu.dk>
    
    * added flatten method
    
    * Add flatten unit test
    
    * implemented flatten
    
    * Work in IsSecure
    
    * Add IsSecure and IsSecure test
    
    * Add WordCount Test
    
    * Add WordCount method
    
    * Add extra IsSecure test
    
    * Add 10 wizards to .csv file
    
    * Add LINQ Queries & Querie tests
    
    Co-authored-by: Daniel <dlli@itu.dk>

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,14 +1,72 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BDSA2021.Assignment3.Tests
 {
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void getRowlingWizards_given_wizards_returns_only_HP_wizards()
         {
+            var actual = Queries.getRowlingWizards(Wizard.Wizards);
+            var expected = new List<string>{"Harry Potter", "Ron Weasley", "Hermione Granger", "Albus Dumbledore"};
+            Assert.Equal(expected, actual);
+        }
 
+        [Fact]
+        public void getRowlingWizardsLINQ_given_wizards_returns_only_HP_wizards()
+        {
+            var actual = Queries.getRowlingWizardsLINQ(Wizard.Wizards);
+            var expected = new List<string>{"Harry Potter", "Ron Weasley", "Hermione Granger", "Albus Dumbledore"};
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void getFirstSithLordYear_given_wizards_return_1977()
+        {
+            var actual = Queries.getFirstSithLordYear(Wizard.Wizards);
+            Assert.Equal(1977, actual);
+        }
+
+        [Fact]
+        public void getFirstSithLordLINQ_given_wizards_return_1977()
+        {
+            var actual = Queries.getFirstSithLordYearLINQ(Wizard.Wizards);
+            Assert.Equal(1977, actual);
+        }
+
+        [Fact]
+        public void getHarryPotterWizards_given_wizards_return_unique_HP_wizards_and_year()
+        {
+            var actual = Queries.getHarryPotterWizards(Wizard.Wizards);
+            var expected = new List<(string,int?)>{("Harry Potter",1997), ("Ron Weasley",1997), ("Hermione Granger",1997), ("Albus Dumbledore",1997)};
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void getHarryPotterWizardsLINQ_given_wizards_return_unique_HP_wizards_and_year()
+        {
+            var actual = Queries.getHarryPotterWizardsLINQ(Wizard.Wizards);
+            var expected = new List<(string,int?)>{("Harry Potter",1997), ("Ron Weasley",1997), ("Hermione Granger",1997), ("Albus Dumbledore",1997)};
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void getSortedWizards_given_wizards_return_yoda_last()
+        {
+            var actual = Queries.getSortedWizards(Wizard.Wizards).Last();
+            var expected = "Yoda";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void getSortedWizardsLINQ_given_wizards_return_yoda_last()
+        {
+            var actual = Queries.getSortedWizardsLINQ(Wizard.Wizards).Last();
+            var expected = "Yoda";
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Assignment3.Tests/WizardTests.cs
+++ Assignment3.Tests/WizardTests.cs
@@ -5,16 +5,26 @@ namespace BDSA2021.Assignment3.Tests
     public class WizardTests
     {
         [Fact]
-        public void Wizards_contains_2_wizards()
+        public void Wizards_contains_12_wizards()
         {
             var wizards = Wizard.Wizards.Value;
 
-            Assert.Equal(2, wizards.Count);
+            Assert.Equal(12, wizards.Count);
         }
 
         [Theory]
         [InlineData("Darth Vader", "Star Wars", 1977, "George Lucas")]
         [InlineData("Sauron", "The Fellowship of the Ring", 1954, "J.R.R. Tolkien")]
+        [InlineData("Darth Maul","Star Wars",1999,"George Lucas")]
+        [InlineData("Yoda","Star Wars",1980,"George Lucas")]
+        [InlineData("Obi-Wan Kenobi","Star Wars",1977,"George Lucas")]
+        [InlineData("Harry Potter","Harry Potter",1997,"J.K. Rowling")]
+        [InlineData("Ron Weasley","Harry Potter",1997,"J.K. Rowling")]
+        [InlineData("Hermione Granger","Harry Potter",1997,"J.K. Rowling")]
+        [InlineData("Albus Dumbledore","Harry Potter",1997,"J.K. Rowling")]
+        [InlineData("Gandalf","The Hobbit",1937,"J.R.R. Tolkien")]
+        [InlineData("Doctor Stephen Strange","Strange Tales",1963,"Stan Lee & Steve Ditko")]
+        [InlineData("Ganon","The Legend of Zelda",1986,"Shigeru Miyamoto")]
         public void Spot_check_wizards(string name, string medium, int year, string creator)
         {
             var wizards = Wizard.Wizards.Value;
